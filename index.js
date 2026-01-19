4cf88048da2dd64b3af1192c9a7db0451c262de9
Md. Rishan Ahmed <166410540+Rishan757@users.noreply.github.com>
2026-01-19 18:26:48 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// link data
+const data = require("./friends_data.json");
+console.log(data);
+
+console.log(data.friends[0].Name); // Output Sakib
+console.log(data.friends[0].Age);  // Output 24
+console.log(data.friends[1].Name); // Output Tanveer
+console.log(data.friends[1].Age); // Output 25
